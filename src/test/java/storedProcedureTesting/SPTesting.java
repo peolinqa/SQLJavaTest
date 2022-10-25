b89145385a8a9b556ca4444457ed45eb2dd970ac
pojirakula <pojirakula@gmail.com>
2022-10-24 17:04:23 -0700

    commit

--- src/test/java/storedProcedureTesting/SPTesting.java
+++ src/test/java/storedProcedureTesting/SPTesting.java
@@ -1,14 +1,27 @@
 package storedProcedureTesting;
 
+import java.sql.*;
+
+import org.apache.commons.lang3.StringUtils;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+
+/**
+ * Syntax                          Stored procedures
+ * { call procedure name() }        Accept no parameters and return no value
+ * { call procedure name(?,?) }     Accept two parameters and return no value
+ * { ?= call procedure name() }     Accept no parameter and return value
+ * { ?= call procedure name(?) }    Accept one parameter and return value
+ */
 
 public class SPTesting {
     Connection con = null;
+    Statement stmt = null;
+    ResultSet rs;
+    CallableStatement cStmt;
 
     @BeforeClass
     void setup() throws SQLException {
@@ -19,5 +32,34 @@ public class SPTesting {
     @AfterClass
     void tearDown() throws SQLException {
         con.close();
+    }
+
+    @Test(priority = 1)
+    void test_storedProceduresExist() throws SQLException {
+        stmt = con.createStatement();
+        rs = stmt.executeQuery("SHOW PROCEDURE STATUS WHERE Name = 'SelectAllCustomers'");
+        rs.next();
+
+        Assert.assertEquals(rs.getString("Name"), "SelectAllCustomers");
+    }
+
+
+    @Test(priority = 2)
+    void test_SelectAllCustomers() throws SQLException {
+        cStmt = con.prepareCall("{CALL SelectAllCustomers()}");
+        cStmt.executeQuery();
+    }
+
+    public boolean compareResultSets(ResultSet resultSet1, ResultSet resultSet2) throws SQLException {
+        while (resultSet1.next()) {
+            resultSet2.next();
+            int count = resultSet1.getMetaData().getColumnCount();
+            for (int i = 1; i <= count; i++) {
+                if (!StringUtils.equals(resultSet1.getString(i), resultSet2.getString(i))) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
